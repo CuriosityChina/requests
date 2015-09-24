8ea6f2f32f595074ff478b443e95f3ccb8eb82d0
Zhanpeng Chen <lowstz@gmail.com>
2015-09-24 15:29:07 +0800

    - fixed more bug and add unit test fixed ConvertResponseToBytes read io.Readcloser fixed httpCall logical confusion

--- requests.go
+++ requests.go
@@ -1,19 +1,29 @@
 package requests
 
 import (
+	"bytes"
 	"encoding/json"
 	"encoding/xml"
 	"errors"
-	"github.com/astaxie/beego/httplib"
-	"github.com/facebookgo/stackerr"
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/astaxie/beego/httplib"
+	"github.com/facebookgo/stackerr"
 )
+
+type loopReader struct {
+	*bytes.Buffer
+}
+
+func (r loopReader) Close() error {
+	return nil
+}
 
 func Parse2Struct(contentType string, data []byte, response interface{}) error {
 	if strings.Contains(contentType, "xml") {
-		return stackerr.Wrap(json.Unmarshal(data, &response))
+		return stackerr.Wrap(xml.Unmarshal(data, &response))
 	}
 	if strings.Contains(contentType, "text/") {
 		switch response.(type) {
@@ -40,25 +50,28 @@ func Parse2Bytes(contentType string, request interface{}) ([]byte, error) {
 }
 
 func ConvertResponseToBytes(r *http.Response) ([]byte, error) {
-	return ioutil.ReadAll(r.Body)
+	buf, err := ioutil.ReadAll(r.Body)
+	origin := loopReader{bytes.NewBuffer(buf)}
+	r.Body = origin
+	return buf, err
 }
 
-func Post(url string, request interface{}, headers map[string]string, response ...interface{}) (*http.Response, error) {
-	return _x("POST", url, request, headers, response)
+func Post(url string, request interface{}, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("POST", url, request, headers, response)
 }
 
-func Put(url string, request interface{}, headers map[string]string, response ...interface{}) (*http.Response, error) {
-	return _x("PUT", url, request, headers, response)
+func Put(url string, request interface{}, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("PUT", url, request, headers, response)
 }
 
-func Delete(url string, request interface{}, headers map[string]string, response ...interface{}) (*http.Response, error) {
-	return _x("DELETE", url, request, headers, response)
+func Delete(url string, request interface{}, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("DELETE", url, request, headers, response)
 }
-func Get(url string, headers map[string]string, response ...interface{}) (*http.Response, error) {
-	return _x("GET", url, nil, headers, response...)
+func Get(url string, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("GET", url, nil, headers, response)
 }
 
-func _x(action, url string, request interface{}, headers map[string]string, response ...interface{}) (*http.Response, error) {
+func httpCall(action, url string, request interface{}, headers map[string]string, response ...interface{}) (*http.Response, error) {
 	var req *httplib.BeegoHttpRequest
 	switch strings.ToUpper(action) {
 	case "PUT":
@@ -83,27 +96,27 @@ func _x(action, url string, request interface{}, headers map[string]string, resp
 		req.Header(k, v)
 	}
 
+	// 转成struct
+	if action != "GET" {
+		result, err := Parse2Bytes(headers["Content-Type"], request)
+		if err != nil {
+			return nil, stackerr.Wrap(err)
+		}
+		req.Body(result)
+	}
+
 	resp, err := req.SendOut()
 	if err != nil {
 		return nil, stackerr.Wrap(err)
 	}
 
-	data, err := req.Bytes()
+	data, err := ConvertResponseToBytes(resp)
 	if err != nil {
 		return resp, stackerr.Wrap(err)
 	}
 
-	if len(response) == 0 {
+	if len(data) == 0 {
 		return resp, errors.New("LENGTH IS 0")
-	}
-
-	// 转成struct
-	if action != "GET" {
-		result, err := Parse2Bytes(headers["Content-Type"], request)
-		if err != nil {
-			return resp, stackerr.Wrap(err)
-		}
-		req.Body(result)
 	}
 
 	err = Parse2Struct(resp.Header.Get("Content-Type"), data, response[0])
@@ -111,5 +124,4 @@ func _x(action, url string, request interface{}, headers map[string]string, resp
 		return resp, stackerr.Wrap(err)
 	}
 	return resp, nil
-
 }
--- requests_test.go
+++ requests_test.go
@@ -1,32 +1,153 @@
 package requests
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
 )
 
+type httpBin struct {
+	Args    struct{} `json:"args"`
+	Data    string   `json:"data"`
+	Files   struct{} `json:"files"`
+	Form    struct{} `json:"form"`
+	Headers struct {
+		Accept_Encoding string `json:"Accept-Encoding"`
+		Content_Length  string `json:"Content-Length"`
+		Content_Type    string `json:"Content-Type"`
+		Host            string `json:"Host"`
+		User_Agent      string `json:"User-Agent"`
+	} `json:"headers"`
+	JSON struct {
+		Hello string `json:"hello"`
+	} `json:"json"`
+	Origin string `json:"origin"`
+	URL    string `json:"url"`
+}
+
+type echo struct {
+	Hello string `json:"hello"`
+}
+
+func TestParse2Struct(t *testing.T) {
+	var bin httpBin
+	testJSON := `
+	{
+		"args": {},
+		"data": "{\"hello\":\"world\"}",
+		"files": {},
+		"form": {},
+		"headers": {
+		"Accept-Encoding": "gzip",
+		"Content-Length": "17",
+		"Content-Type": "application/json; charset=utf-8",
+		"Host": "httpbin.org",
+		"User-Agent": "beegoServer"
+		},
+		"json": {
+			"hello": "world"
+		},
+		"origin": "118.244.254.30",
+		"url": "http://httpbin.org/post"
+	}
+	`
+	err := Parse2Struct("json", []byte(testJSON), &bin)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != "world" {
+		t.Errorf("want world, got %s", bin.JSON.Hello)
+	}
+}
+
+func TestParse2Bytes(t *testing.T) {
+	var bin httpBin
+	bin.Headers.Content_Length = "application/json; charset=utf-8"
+	b, err := Parse2Bytes("json", bin)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.Contains(string(b), "application/json; charset=utf-8") {
+		t.Errorf("want true, got %t", strings.Contains(string(b), "application/json; charset=utf-8"))
+	}
+}
+
+func TestConvertResponseToBytes(t *testing.T) {
+	resp, err := http.Get("https://api.github.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+	b1, err := ConvertResponseToBytes(resp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b2, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b1) != string(b2) {
+		t.Errorf("want true, got %t", string(b1) == string(b2))
+	}
+}
+
 func TestGet(t *testing.T) {
-	// var x struct {
-	// 	Code int    `json:"code"`
-	// 	Msg  string `json:"msg"`
-	// }
-	var x interface{}
+	var bin httpBin
 	// var url = "http://www.baidu.com"
-	var url = "http://192.168.9.5:8081/v1/wx/1f6b2a478d4d4678276944b0a607058b/auth/accesstoken"
-	result, err := Get(url, nil, &x)
-	fmt.Println(x, err)
-	m, _ := ConvertResponseToBytes(result)
-	fmt.Println(string(m))
+	var url = "http://httpbin.org/get"
+	result, err := Get(url, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if len(strings.Split(bin.Origin, ".")) != 4 {
+		t.Errorf("want 4, got %d", len(strings.Split(bin.Origin, ".")))
+	}
 }
 
 func TestPost(t *testing.T) {
+	var bin httpBin
+	request := &echo{Hello: "world"}
+	var url = "http://httpbin.org/post"
 
+	result, err := Post(url, request, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != request.Hello {
+		t.Errorf("want %s, got %+v", request.Hello, bin.JSON.Hello)
+	}
 }
 
 func TestDelete(t *testing.T) {
-
+	var bin httpBin
+	request := &echo{Hello: "world"}
+	var url = "http://httpbin.org/delete"
+	result, err := Delete(url, request, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != request.Hello {
+		t.Errorf("want %s, got %+v", request.Hello, bin.JSON.Hello)
+	}
 }
 
 func TestPut(t *testing.T) {
-
+	var bin httpBin
+	request := &echo{Hello: "world"}
+	var url = "http://httpbin.org/put"
+	result, err := Put(url, request, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != request.Hello {
+		t.Errorf("want %s, got %+v", request.Hello, bin.JSON.Hello)
+	}
 }
