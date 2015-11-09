49a959e50b08370763aeb713c5fc59a7bbae59d0
Zhanpeng Chen <lowstz@gmail.com>
2015-11-09 23:41:27 +0800

    public commit

---
+++ requests.go
@@ -0,0 +1,139 @@
+package requests
+
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/astaxie/beego/httplib"
+	"github.com/facebookgo/stackerr"
+)
+
+type loopReader struct {
+	*bytes.Buffer
+}
+
+func (r loopReader) Close() error {
+	return nil
+}
+
+func Parse2Struct(contentType string, data []byte, response interface{}) error {
+	if strings.Contains(contentType, "xml") {
+		return stackerr.Wrap(xml.Unmarshal(data, &response))
+	}
+	if strings.Contains(contentType, "text/") {
+		switch response.(type) {
+		case string:
+			response = string(data)
+		default:
+			response = &data
+		}
+		return nil
+	}
+	return stackerr.Wrap(json.Unmarshal(data, &response))
+}
+
+func Parse2Bytes(contentType string, request interface{}) ([]byte, error) {
+	if strings.Contains(contentType, "json") {
+		result, x := json.Marshal(request)
+		return result, stackerr.Wrap(x)
+	}
+	if strings.Contains(contentType, "xml") {
+		result, x := xml.Marshal(request)
+		return result, stackerr.Wrap(x)
+	}
+	return nil, stackerr.New("NOT FOUND")
+}
+
+func ConvertResponseToBytes(r *http.Response) ([]byte, error) {
+	if r == nil {
+		err := errors.New("http response address nil")
+		return []byte{}, err
+	}
+	buf, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	origin := loopReader{bytes.NewBuffer(buf)}
+	r.Body = origin
+	return buf, err
+}
+
+func Post(url string, request interface{}, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("POST", url, request, headers, response)
+}
+
+func Put(url string, request interface{}, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("PUT", url, request, headers, response)
+}
+
+func Delete(url string, request interface{}, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("DELETE", url, request, headers, response)
+}
+func Get(url string, headers map[string]string, response interface{}) (*http.Response, error) {
+	return httpCall("GET", url, nil, headers, response)
+}
+
+func httpCall(action, url string, request interface{}, headers map[string]string, response ...interface{}) (*http.Response, error) {
+	var req *httplib.BeegoHttpRequest
+	debugf("httpCall method: %s, url %s", action, url)
+	debugf("httpCall default request header: %+v", headers)
+	switch strings.ToUpper(action) {
+	case "PUT":
+		req = httplib.Put(url)
+	case "POST":
+		req = httplib.Post(url)
+	case "DELETE":
+		req = httplib.Delete(url)
+	case "GET":
+		req = httplib.Get(url)
+	default:
+		req = httplib.Post(url)
+	}
+
+	if headers == nil || headers["Content-Type"] == "" {
+		headers = map[string]string{
+			"Content-Type": "application/json; charset=utf-8",
+		}
+	}
+
+	debugf("httpCall request header: %+v", headers)
+
+	for k, v := range headers {
+		req.Header(k, v)
+	}
+
+	// 转成struct
+	if action != "GET" {
+		result, err := Parse2Bytes(headers["Content-Type"], request)
+		if err != nil {
+			return nil, stackerr.Wrap(err)
+		}
+		debugf("httpCall send body: %s", string(result))
+		req.Body(result)
+	}
+
+	resp, err := req.SendOut()
+	if err != nil {
+		return nil, stackerr.Wrap(err)
+	}
+	debugf("httpCall response header %+v", resp.Header)
+	data, err := ConvertResponseToBytes(resp)
+	if err != nil {
+		return resp, stackerr.Wrap(err)
+	}
+	debugf("httpCall response body: %s", string(data))
+
+	if len(data) == 0 {
+		return resp, errors.New("LENGTH IS 0")
+	}
+
+	err = Parse2Struct(resp.Header.Get("Content-Type"), data, response[0])
+	if err != nil {
+		return resp, stackerr.Wrap(err)
+	}
+	debugf("httpCall response struct: %+v", response[0])
+	return resp, nil
+}
---
+++ requests_test.go
@@ -0,0 +1,203 @@
+package requests
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+type httpBin struct {
+	Args    struct{} `json:"args"`
+	Data    string   `json:"data"`
+	Files   struct{} `json:"files"`
+	Form    struct{} `json:"form"`
+	Headers struct {
+		Accept_Encoding string `json:"Accept-Encoding"`
+		Content_Length  string `json:"Content-Length"`
+		Content_Type    string `json:"Content-Type"`
+		Host            string `json:"Host"`
+		User_Agent      string `json:"User-Agent"`
+	} `json:"headers"`
+	JSON struct {
+		Hello string `json:"hello"`
+	} `json:"json"`
+	Origin string `json:"origin"`
+	URL    string `json:"url"`
+}
+
+type httpBinXML struct {
+	Slideshow struct {
+		Title  string `xml:"title,attr"`
+		Date   string `xml:"date,attr"`
+		Author string `xml:"author,attr"`
+		Slide  []struct {
+			Type  string   `xml:"type,attr"`
+			Title string   `xml:"title"`
+			Item  []string `xml:"item"`
+		} `xml:"slide"`
+	} `xml:"slideshow"`
+}
+
+type echo struct {
+	Hello string `json:"hello"`
+}
+
+func TestParse2StructWithJSON(t *testing.T) {
+	var bin httpBin
+	testJSON := `
+	{
+		"args": {},
+		"data": "{\"hello\":\"world\"}",
+		"files": {},
+		"form": {},
+		"headers": {
+		"Accept-Encoding": "gzip",
+		"Content-Length": "17",
+		"Content-Type": "application/json; charset=utf-8",
+		"Host": "httpbin.org",
+		"User-Agent": "beegoServer"
+		},
+		"json": {
+			"hello": "world"
+		},
+		"origin": "118.244.254.30",
+		"url": "http://httpbin.org/post"
+	}
+	`
+	err := Parse2Struct("json", []byte(testJSON), &bin)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != "world" {
+		t.Errorf("want world, got %s", bin.JSON.Hello)
+	}
+}
+
+func TestParse2StructWithXML(t *testing.T) {
+	var binXML httpBinXML
+	testXML := `
+	<?xml version='1.0' encoding='utf-8'?>
+
+	<!--  A SAMPLE set of slides  -->
+
+	<slideshow
+    	title="Sample Slide Show"
+    	date="Date of publication"
+    	author="Yours Truly"
+    >
+
+    <!-- TITLE SLIDE -->
+    <slide type="all">
+      <title>Wake up to WonderWidgets!</title>
+    </slide>
+
+    <!-- OVERVIEW -->
+    <slide type="all">
+        <title>Overview</title>
+        <item>Why <em>WonderWidgets</em> are great</item>
+        <item/>
+        <item>Who <em>buys</em> WonderWidgets</item>
+    </slide>
+
+	</slideshow>
+	`
+	err := Parse2Struct("xml", []byte(testXML), binXML)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParse2StructWithText(t *testing.T) {
+
+}
+
+func TestParse2Bytes(t *testing.T) {
+	var bin httpBin
+	bin.Headers.Content_Length = "application/json; charset=utf-8"
+	b, err := Parse2Bytes("json", bin)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.Contains(string(b), "application/json; charset=utf-8") {
+		t.Errorf("want true, got %t", strings.Contains(string(b), "application/json; charset=utf-8"))
+	}
+}
+
+func TestConvertResponseToBytes(t *testing.T) {
+	resp, err := http.Get("https://api.github.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+	b1, err := ConvertResponseToBytes(resp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b2, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b1) != string(b2) {
+		t.Errorf("want true, got %t", string(b1) == string(b2))
+	}
+}
+
+func TestGet(t *testing.T) {
+	var bin httpBin
+	var url = "http://httpbin.org/get"
+	result, err := Get(url, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if len(strings.Split(bin.Origin, ".")) != 4 {
+		t.Errorf("want 4, got %d", len(strings.Split(bin.Origin, ".")))
+	}
+}
+
+func TestPost(t *testing.T) {
+	var bin httpBin
+	request := &echo{Hello: "world"}
+	var url = "http://httpbin.org/post"
+
+	result, err := Post(url, request, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != request.Hello {
+		t.Errorf("want %s, got %+v", request.Hello, bin.JSON.Hello)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	var bin httpBin
+	request := &echo{Hello: "world"}
+	var url = "http://httpbin.org/delete"
+	result, err := Delete(url, request, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != request.Hello {
+		t.Errorf("want %s, got %+v", request.Hello, bin.JSON.Hello)
+	}
+}
+
+func TestPut(t *testing.T) {
+	var bin httpBin
+	request := &echo{Hello: "world"}
+	var url = "http://httpbin.org/put"
+	result, err := Put(url, request, nil, &bin)
+	if err != nil {
+		m, _ := ConvertResponseToBytes(result)
+		t.Log(string(m))
+		t.Fatal(err)
+	}
+	if bin.JSON.Hello != request.Hello {
+		t.Errorf("want %s, got %+v", request.Hello, bin.JSON.Hello)
+	}
+}
